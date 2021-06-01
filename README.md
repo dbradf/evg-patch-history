5c9a73c899e90446c112335460252e784ab82d88
David Bradford <david.bradford@mongodb.com>
2021-05-31 21:56:00 -0400

    Add structopt

---
+++ README.md
@@ -0,0 +1,11 @@
+# Evg Patch History
+
+A tool to generate a CSV of patches run against an Evergreen project.
+
+## Usage
+
+```
+$ evg-patch-history --project-id mongodb-mongo-master --output-file patches.csv
+$ ls patches.csv
+patches.csv
+```
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 use csv::WriterBuilder;
 use serde::{Deserialize, Serialize};
 use std::{error::Error, sync::mpsc::{self, Receiver}, thread};
+use structopt::StructOpt;
 use chrono::{Duration, Utc};
 use evg_api_rs::EvgClient;
 use graphql_client::GraphQLQuery;
 use futures::{StreamExt, future::join_all, pin_mut};
-
-const BATCH_SIZE: usize = 50;
-
 
 #[derive(Debug, Serialize)]
 pub struct Record {
@@ -31,7 +29,8 @@ pub struct APatch {
     pub description: String,
     pub author: String,
     pub alias: String,
-    pub variantsTasks: Vec<VariantsTasks>,
+    #[serde(rename = "variantsTasks")]
+    pub variants_tasks: Vec<VariantsTasks>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -53,22 +52,43 @@ pub struct PatchResponse {
 )]
 pub struct PatchDetails;
 
+#[derive(Debug, StructOpt)]
+#[structopt(about = "Generate a CSV of patches run on the given projects.")]
+struct Opt {
+    /// Evergreen project ID.
+    #[structopt(long)]
+    project_id: String,
+
+    /// Number of weeks of data to gather.
+    #[structopt(long, default_value = "1")]
+    weeks_back: i64,
+
+    /// Number of patches to query at a time.
+    #[structopt(long, default_value = "50")]
+    batch_size: usize,
+
+    /// Name of output file to write.
+    #[structopt(long, default_value = "patches.csv")]
+    output_file: String,
+}
+
 #[tokio::main]
 async fn main() {
-    let project = std::env::args().nth(1).expect("Expected project id");
-    let weeks_back = std::env::args().nth(2).unwrap_or_else(|| String::from("1")).parse::<i64>().expect("Weeks back should be an number");
+    let opt = Opt::from_args();
 
     let evg_client = EvgClient::new().unwrap();
-    let patch_stream = evg_client.stream_project_patches(&project, None).await;
+    let patch_stream = evg_client.stream_project_patches(&opt.project_id, None).await;
     pin_mut!(patch_stream);
 
     let (tx, rx) = mpsc::channel();
 
+    let batch_size = opt.batch_size;
+    let output_file = opt.output_file.to_string();
     let handle = thread::spawn(move || {
-        patch_collector(rx);
+        patch_collector(rx, batch_size, &output_file);
     });
 
-    let lookback = Utc::now() - Duration::weeks(weeks_back);
+    let lookback = Utc::now() - Duration::weeks(opt.weeks_back);
     let mut count = 0;
 
     while let Some(patch) = patch_stream.next().await {
@@ -97,7 +117,7 @@ enum Action {
 }
 
 #[tokio::main]
-async fn patch_collector(rx: Receiver<Action>) {
+async fn patch_collector(rx: Receiver<Action>, batch_size: usize, output_file: &str) {
     let evg_client = EvgClient::new().unwrap();
 
     let mut patch_ids = vec![];
@@ -107,7 +127,7 @@ async fn patch_collector(rx: Receiver<Action>) {
         match event {
             Action::Patch(patch_id) => {
                 patch_ids.push(patch_id);
-                if patch_ids.len() >= BATCH_SIZE {
+                if patch_ids.len() >= batch_size {
                     count += 1;
                     println!("Sending batch: {}", count);
                     let patches = join_all(patch_ids.iter().map(|p| get_patch(&evg_client, p))).await;
@@ -127,7 +147,7 @@ async fn patch_collector(rx: Receiver<Action>) {
         }
     }
 
-    let mut wtr = WriterBuilder::new().from_path("patches.csv").unwrap();
+    let mut wtr = WriterBuilder::new().from_path(output_file).unwrap();
     records.iter().for_each(|r| wtr.serialize(r).unwrap());
     wtr.flush().unwrap();
 }
@@ -140,7 +160,7 @@ fn process_patches(patches: &[Result<PatchResponse, Box<dyn Error>>]) -> Vec<Rec
                 if patch.data.patch.alias == "__commit_queue" {
                     continue;
                 }
-                for bv in &patch.data.patch.variantsTasks {
+                for bv in &patch.data.patch.variants_tasks {
                     records.push(Record {
                         id: patch.data.patch.id.to_string(),
                         author: patch.data.patch.author.to_string(),
